22766f981a37094f4c17372b2a4eff2171945f72
RehamElaraby <rehamelaraby2272@gmail.com>
2024-02-20 02:12:28 +0200

    .

--- socialapp/lib/main.dart
+++ socialapp/lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:socialapp/screens/login/cubit/loginCubit.dart';
+import 'package:socialapp/screens/register/cubit/registerCubit.dart';
 
 import 'firebase_options.dart';
+import 'screens/login/cubit/blocObserver.dart';
 import 'screens/login/loginScreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  Bloc.observer = MyBlocObserver();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(
+    MultiBlocProvider(
+      child: MyApp(),
+      providers: [
+        BlocProvider(create: ((context) => LoginCubit())),
+        BlocProvider(create: ((context) => RegisterCubit())),
+        // BlocProvider(create: ((context) => SearchCubit())),
+      ],
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
